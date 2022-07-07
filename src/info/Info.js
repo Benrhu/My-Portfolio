7a9ba9f0a3c5471cd7527d196a37cd4afc38b7cb
Benrhu <rubendiaz300000@gmail.com>
2022-07-07 14:29:27 +0200

    My Portfolio

---
+++ src/info/Info.js
@@ -0,0 +1,103 @@
+import me from "../img/me.jpg"
+import img1 from "../img/oven.jpg"
+import img2 from "../img/mock2.png"
+
+export let colors = [
+    "rgb(0,255,164)", 
+    "rgb(166,104,255)"
+];
+
+export const info = {
+    firstName: "Ruben",
+    lastName: "Díaz",
+    initials: "TS", 
+    position: "a Full Stack Developer",
+    selfPortrait: me,
+    gradient: `-webkit-linear-gradient(135deg, ${colors})`,
+    baseColor: colors[0],
+    miniBio: [ 
+        {
+            emoji: '🔥',
+            text: 'fueled by passion'
+        },
+        {
+            emoji: '🌎',
+            text: 'based in Spain'
+        },
+        {
+            emoji: "💼",
+            text: "Junior Java Development"
+        },
+        {
+            emoji: "📧",
+            text: "rubendiaz300000@gmail.com"
+        }
+    ],
+    socials: [
+        {
+            link: "https://github.com/Benrhu",
+            icon: "fa fa-github"
+        },
+        {
+            link: "https://linkedin.com/rubendiazhuelva",
+            icon: "fa fa-linkedin"
+        },
+        {
+            link: "https://www.open-bootcamp.com",
+            icon: "fa fa-graduation-cap"
+        }
+
+    ],
+    bio: "Hello! I'm Rubén. I'm a backend developer. I studied at OpenBootCamp.com, I enjoy long walks on the mountain and I believe blockchain technology will inevitably and forever change the way we relate to and use money. You should hire me :)",
+    skills:
+        {
+            experienceWith: [
+                'java', 
+                'spring', 
+                'node', 
+                'git', 
+                'github', 
+                'html5', 
+                'css3', 
+                'figma',
+                'Angular'
+            ],
+            exposedTo: [
+                'c#', 
+                'python', 
+                'unity'
+            ]
+        }
+    ,
+    hobbies: [
+        {
+            label: 'theater',
+            emoji: '🎭'
+        },
+        {
+            label: 'philosophy',
+            emoji: '🤔'
+        },
+        {
+            label: 'reading',
+            emoji: '📖'
+        },
+        {
+            label: 'kungfu',
+            emoji: '👊'
+        },
+    ],
+    portfolio: [
+        {
+            title: "Ovenex",
+            source: "https://github.com/Benrhu/Ovenex", // this should be a link to the **repository** of the project, where the code is hosted.
+            image: img1
+        },
+        {
+            title: "Project 2",
+            live: "",
+            source: "",
+            image: img2
+        },
+    ]
+}
